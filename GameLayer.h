fe9967a82bbce422ac370ab44ee84e522097a77d
DohyunJo <edan1252@gmail.com>
2014-12-16 17:08:08 +0900

    Update GameLayer.h

--- GameLayer.h
+++ GameLayer.h
@@ -4,25 +4,36 @@
 #include "cocos2d.h"
 #include "../Background.h"
 #include "../GameController.h"
+#include "../fighter/Flight.h"
+#include "../object/Bullet.h"
 #define STATE_PLAYING 0
 #define STATE_GAMEOVER 1
 #define STATE_CLEAR 2
 #define MAX_CONTAINT_WIDTH 40
 #define MAX_CONTAINT_HEIGHT 40
-
+#define Z_PLAYER 5
+#define Z_BACKGROUND 1
+#define Z_ENEMY 3
+#define Z_BULLET 4
 
 
 USING_NS_CC;
+
+class Flight;
+class Bullet;
 
 class GameLayer : public cocos2d::Layer
 {
 private:
 	SpriteBatchNode* _shipBatch;
 	SpriteBatchNode* _backgroundBatch;
+	SpriteBatchNode* _bulletBatch;
 	Background* _background;
 	int _state;
 	cocos2d::Label *a = NULL;
-	bool key_UP, key_DOWN, key_LEFT, key_RIGHT;
+	Flight* _flight;
+	Rect screenRect;
+	//Label *lb;
 public:
 	GameLayer();
 
@@ -42,21 +53,26 @@ public:
 	}
 
 	void OnAcceleration(cocos2d::Acceleration *acc, cocos2d::Event *event);
+	//bool OnTouchBegan(cocos2d::Touch *touch, cocos2d::Event * event);
+	void OnTouchMoved(cocos2d::Touch *touch, cocos2d::Event * event);
+	void OnTouchEnded(cocos2d::Touch *touch, cocos2d::Event * event);
 
-	bool isKeyDown() const {
-		return key_DOWN;
+	void addBullet(Bullet* bullet);
+
+	const Rect& getScreenRect() const {
+		return screenRect;
 	}
 
-	bool isKeyLeft() const {
-		return key_LEFT;
+	void setScreenRect(const Rect& screenRect) {
+		this->screenRect = screenRect;
 	}
 
-	bool isKeyRight() const {
-		return key_RIGHT;
+	Flight*& getFlight() {
+		return _flight;
 	}
 
-	bool isKeyUp() const {
-		return key_UP;
+	void setFlight(Flight*& flight) {
+		_flight = flight;
 	}
 };
 
