47171248f1110079d0ae3bcb27ebbde348089422
DohyunJo <edan1252@gmail.com>
2014-12-16 17:07:44 +0900

    Update GameLayer.cpp

--- GameLayer.cpp
+++ GameLayer.cpp
@@ -19,6 +19,7 @@ GameLayer::GameLayer()
 	SpriteFrameCache::getInstance()->addSpriteFramesWithFile("images/ship3.plist");
 	SpriteFrameCache::getInstance()->addSpriteFramesWithFile("images/background.plist");
 
+		SpriteFrameCache::getInstance()->addSpriteFramesWithFile("images/ship1.plist");
 	// cache background
 	auto tmpBackground = TextureCache::getInstance()->addImage("images/background.png");
 	_backgroundBatch = SpriteBatchNode::createWithTexture(tmpBackground);
@@ -29,7 +30,17 @@ GameLayer::GameLayer()
 	_shipBatch = SpriteBatchNode::createWithTexture(tmpShip);
 	this->addChild(_shipBatch);
 
+	auto tmpBullet = TextureCache::getInstance()->addImage("images/s1enemy.png");
+	_bulletBatch = SpriteBatchNode::createWithTexture(tmpBullet);
+	this->addChild(_bulletBatch, 4);
 
+	Size winSize = Director::getInstance()->getWinSize();
+	this->screenRect = Rect(0, 0, winSize.width, winSize.height);
+
+/*	lb = Label::create("", "arial", 50);
+	lb->setAnchorPoint(ccp(0.5, 0.5));
+	lb->setPosition(Point(100, 100));
+	this->addChild(lb, 10);*/
 
 
 	// cache F117
@@ -50,8 +61,6 @@ bool GameLayer::init()
 	CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("sound/sfx/sfx_006.mp3", false);
 	CocosDenshion::SimpleAudioEngine::sharedEngine()->playBackgroundMusic("sound/bg/bg_stage1.mp3", true);
 
-x
-
 	initBackground();
 	this->scheduleUpdate();
 
@@ -59,9 +68,17 @@ x
 	auto listener = EventListenerAcceleration::create(CC_CALLBACK_2(GameLayer::OnAcceleration, this));
 	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
 
+	auto listener_touch = EventListenerTouchOneByOne::create();
+	listener_touch->setSwallowTouches(true);
+//	listener->onTouchBegan = CC_CALLBACK_2(GameLayer::onTouchBegan, this);
+	listener_touch->onTouchMoved = CC_CALLBACK_2(GameLayer::onTouchMoved, this);
+	listener_touch->onTouchEnded = CC_CALLBACK_2(GameLayer::onTouchEnded, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener_touch, this);
 
-	auto f22 = new Flight();
-	this->addChild(f22, 4);
+	_flight = new Flight();
+	this->addChild(_flight, 4);
+
+
 
 	//auto f117 = new F117();
 	//this->addChild(f117);
@@ -72,47 +89,86 @@ x
 
 void GameLayer::OnAcceleration(cocos2d::Acceleration *acc, cocos2d::Event *event)
 {// x : -0.25 ~ 0.25	y : Down : -0.70 ~ -30 : Up
-
-	if(acc->x < -0.25)
+	double dx, dy;
+	if(acc->x < -0.15)
 	{
+
 		//왼쪽
-		this->key_LEFT = true;
-		this->key_RIGHT = false;
+		_flight->setKeyLeft(true);
+		_flight->setKeyRight(false);
+
 	}
-	else if(acc->x > 0.25)
+	else if(acc->x > 0.15)
 	{
 		//오른쪽
-		this->key_LEFT = false;
-		this->key_RIGHT = true;
+		_flight->setKeyLeft(false);
+		_flight->setKeyRight(true);
+
 	}
 	else
 	{
 		//평지
-		this->key_LEFT = false;
-		this->key_RIGHT = false;
+		_flight->setKeyLeft(false);
+		_flight->setKeyRight(false);
+
 	}
 
-	if(acc->y < -0.75)
+	if(acc->y < -0.5)
 	{
 		//아래로
-		this->key_UP = false;
-		this->key_DOWN = true;
+		_flight->setKeyUp(false);
+		_flight->setKeyDown(true);
+
 	}
-	else if(acc->y > -0.3)
+	else if(acc->y > -0.5)
 	{
 		//위로
-		this->key_UP = true;
-		this->key_DOWN = false;
+		_flight->setKeyUp(true);
+		_flight->setKeyDown(false);
+
 	}
 	else
 	{
 		//평지
-		this->key_UP = false;
-		this->key_DOWN = false;
-	}
+		_flight->setKeyUp(false);
+		_flight->setKeyDown(false);
 
+	}
+	dx = abs(acc->x) * 7.2f;
+	dy = abs(-0.5f - acc->y) * 8.0f;
+
+	_flight->setDeltaXY(dx, dy);
+	//std::to_string(str + str1);
+	//lb->setString(str + str1);
 }
 
+void GameLayer::OnTouchEnded(cocos2d::Touch *touch, cocos2d::Event *event)
+{
+	_flight->setKeyShoot(false);
+	_flight->setKeyBoom(false);
+
+}
+void GameLayer::OnTouchMoved(cocos2d::Touch *touch, cocos2d::Event *event)
+{
+	Point GetPos = touch->getLocation();
+	if(GetPos.x < 160)
+	{
+		_flight->setKeyBoom(true);
+		_flight->setKeyShoot(false);
+		//폭탄
+	}
+	else if(_flight->isKeyBoom() == false && GetPos.x >= 160)
+	{
+		_flight->setKeyShoot(true);
+
+		//촐알
+	}
+}
+
+void GameLayer::addBullet(Bullet* bullet)
+{
+	this->_bulletBatch->addChild(bullet, Z_BULLET);
+}
 void GameLayer::update(float dt)
 {
 
